365e11ead87e41ab9a67e3c85c486bb4f378f258
aktrooper9 <andrewkim.bc@gmail.com>
2023-07-21 16:53:18 -0700

    I made a turtle command shape

--- _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
+++ _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
@@ -15,12 +15,24 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # TODO 1) Create a new Turtle
+    jack = turtle.Turtle()
     #      2) Make the turtle draw a shape (this will take more than one line
     #         of code)
+    jack.forward(100)
+    jack.left(90)
+    jack.forward(100)
+    jack.left(90)
+    jack.forward(100)
+    jack.left(90)
+    jack.forward(100)
     #      3) Set the pen width to 10
+    chicken = turtle.pensize(10)
     #      4) Ask the user what color pen they would like to draw with
+    pen = simpledialog.askstring(title=None, prompt="What color pen")
     #      5) Use an if/else statement to set the pen color that the user
     #         requested
+    if pen == "Red":
+        fill()
     #      6) If the user doesn't enter anything, choose a random color
     #      7) Put a loop around your code so that you keep asking the user for
     #         more colors & drawing them
