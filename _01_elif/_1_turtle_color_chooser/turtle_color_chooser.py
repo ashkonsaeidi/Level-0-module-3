071961dfcd5b502a546ede1ff848e520bb68f2b3
aktrooper9 <54760319+OceanNut420@users.noreply.github.com>
2023-07-28 16:53:08 -0700

    I made a turtkle code and a cat code

--- _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
+++ _01_elif/_1_turtle_color_chooser/turtle_color_chooser.py
@@ -15,25 +15,47 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # TODO 1) Create a new Turtle
+
     jack = turtle.Turtle()
     #      2) Make the turtle draw a shape (this will take more than one line
     #         of code)
-    jack.forward(100)
-    jack.left(90)
-    jack.forward(100)
-    jack.left(90)
-    jack.forward(100)
-    jack.left(90)
-    jack.forward(100)
+    for i in range(100):
+        jack.forward(100)
+        jack.left(90)
+        jack.forward(100)
+        jack.left(90)
+        jack.forward(100)
+        jack.left(90)
+        jack.forward(100)
     #      3) Set the pen width to 10
-    chicken = turtle.pensize(10)
+        chicken = turtle.pensize(10)
     #      4) Ask the user what color pen they would like to draw with
-    pen = simpledialog.askstring(title=None, prompt="What color pen")
+        pen = simpledialog.askstring(title=None, prompt="What color pen")
     #      5) Use an if/else statement to set the pen color that the user
     #         requested
-    if pen == "Red":
-        fill()
+        if pen == "Red":
+            jack.pencolor('red')
+        if pen == "Green":
+            jack.pencolor('green')
+        if pen == "Purple":
+            jack.pencolor('purple')
+        if pen == "Orange":
+            jack.pencolor('orange')
+        elif pen == "Violet":
+            jack.pencolor('violet')
+        elif pen == "Blue":
+            jack.pencolor('blue')
+        elif pen == "Yellow":
+            jack.pencolor('yellow')
+        elif pen == "Pink":
+            jack.pencolor('pink')
+        elif pen == "Purple":
+            jack.pencolor('purple')
+        else:
+            get_random_color()
+
     #      6) If the user doesn't enter anything, choose a random color
+
     #      7) Put a loop around your code so that you keep asking the user for
     #         more colors & drawing them
 
